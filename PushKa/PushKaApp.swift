952b4a571b0cb98fb30fb1ab35a9e1fc9864d86f
Developer <Lepiozdyx@gmail.com>
2025-06-09 11:57:26 -0300

    Initial Commit

---
+++ PushKa/PushKaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushKaApp.swift
+//  PushKa
+//
+//  Created by Alex on 09.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct PushKaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
